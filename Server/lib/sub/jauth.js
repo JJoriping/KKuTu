aae9c49da9b2b83b8a6d41943881c81f153820df
JJoriping <op@jjo.kr>
2017-02-15 13:08:56 +0900

    add license comment on source file

--- Server/lib/sub/jauth.js
+++ Server/lib/sub/jauth.js
@@ -1,3 +1,21 @@
+/**
+ * Rule the words! KKuTu Online
+ * Copyright (C) 2017 JJoriping(op@jjo.kr)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const NAVER_ID = "네이버 앱 아이디";
 const NAVER_SECRET = "네이버 앱 시크릿";
 const GOOGLE_ID = "Google App ID";
