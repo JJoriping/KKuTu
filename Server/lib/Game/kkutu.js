55553c363ef29edbd032c763bb75b163806e0fe6
JJoriping <op@jjo.kr>
2017-09-16 01:09:11 +0900

    Fixed issue #149

--- Server/lib/Game/kkutu.js
+++ Server/lib/Game/kkutu.js
@@ -232,7 +232,7 @@ exports.Client = function(socket, profile, sid){
 	}else{
 		gp = guestProfiles[Math.floor(Math.random() * guestProfiles.length)];
 		
-		my.id = "guest:" + sid;
+		my.id = "guest__" + sid;
 		my.guest = true;
 		my.isAjae = false;
 		my.profile = {
