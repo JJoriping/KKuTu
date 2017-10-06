1f48f08255bc06889312d783ee945df5679c5814
small_sunshine <jioo0224@naver.com>
2017-10-06 13:48:38 +0900

    네이버 로그인 수정

--- Server/lib/Web/routes/login.js
+++ Server/lib/Web/routes/login.js
@@ -81,38 +81,25 @@ exports.run = (Server, page) => {
         (req, accessToken, refreshToken, profile, done) => {
             const $p = {};
 
-            Web.post({
-                url: "https://openapi.naver.com/v1/nid/me",
-                headers: { 'Authorization': "Bearer " + accessToken }
-            }, function(err, res, doc){
-                if(err) return done(null, false, { error: 400 });
-                if(!doc) return done(null, false, { error: 500 });
-                try{ doc = JSON.parse(doc); }catch(e){ return done(null, false, { error: 500 }); }
-                
-                if(doc.resultcode == "00"){
-                    $p.type = "naver";
-                    $p.id = doc.response.id;
-                    $p.name = doc.response.name;
-                    $p.title = doc.response.nickname;
-                    $p.image = doc.response.profile_image;
-                    
-                    /* 망할 셧다운제
-                    $p._age = doc.response.age.split('-').map(Number);
-                    $p._age = { min: ($p._age[0] || 0) - 1, max: $p._age[1] - 1 };
-                    $p.birth = doc.response.birthday.split('-').map(Number);
-                    if(MONTH < $p.birth[0] || (MONTH == $p.birth[0] && DATE < $p.birth[1])){
-                        $p._age.min--;
-                        $p._age.max--;
-                    }
-                    $p.isAjae = Ajae($p.birth, $p._age);
-                    */
-                    // $p.sex = doc.response[0].gender[0];
+            $p.type = "naver";
+            $p.id = profile.id;
+            $p.name = profile.displayName;
+            $p.title = profile.displayName;
+            $p.image = profile.profile_image;
+            
+            /* 망할 셧다운제
+            $p._age = profile._json.age.split('-').map(Number);
+            $p._age = { min: ($p._age[0] || 0) - 1, max: $p._age[1] - 1 };
+            $p.birth = profile._json.birthday.split('-').map(Number);
+            if(MONTH < $p.birth[0] || (MONTH == $p.birth[0] && DATE < $p.birth[1])){
+                $p._age.min--;
+                $p._age.max--;
+            }
+            $p.isAjae = Ajae($p.birth, $p._age);
+            */
+            // $p.sex = profile[0].gender[0];
 
-                    process(req, accessToken, MainDB, $p, done);
-                }else{
-                    done(null, false, { error: 401 });
-                }
-            });
+            process(req, accessToken, MainDB, $p, done);
         }
     ));
 
