4b8642b5db6f8eb732167cf9cb35df312b2160a4
horyu1234 <horyu1234@naver.com>
2018-01-07 21:29:35 +0900

    누락된 비동기 처리 추가

--- Server/lib/sub/VendorDBMigration.js
+++ Server/lib/sub/VendorDBMigration.js
@@ -9,13 +9,22 @@ exports.initDatabase = function (_database) {
     database = _database;
 };
 
-function processVendorMigration(userId) {
+function processVendorMigration(userId, callback) {
     let oldUserId = userId.split('-')[1];
 
     hasUser(oldUserId, function (result) {
         if (result) {
-            modifyOldUserId(oldUserId, userId);
-            JLog.info(`${oldUserId} 의 식별 번호가 ${userId} 로 마이그레이션 되었습니다.`);
+            modifyOldUserId(oldUserId, userId, function () {
+                JLog.info(`${oldUserId} 의 식별 번호가 ${userId} 로 마이그레이션 되었습니다.`);
+
+                if (callback !== undefined) {
+                    callback();
+                }
+            });
+        } else {
+            if (callback !== undefined) {
+                callback();
+            }
         }
     });
 }
@@ -36,7 +45,7 @@ function hasUser(userId, callback) {
     })
 }
 
-function modifyOldUserId(oldUserId, userId) {
+function modifyOldUserId(oldUserId, userId, callback) {
     let query = "UPDATE users " +
         "SET _id = '" + userId + "' " +
         "WHERE _id = '" + oldUserId + "';";
@@ -44,6 +53,10 @@ function modifyOldUserId(oldUserId, userId) {
     database.query(query, (err) => {
         if (err) {
             return JLog.error(`Error executing query ${err.stack}`);
+        }
+
+        if (callback !== undefined) {
+            callback();
         }
     })
 }
