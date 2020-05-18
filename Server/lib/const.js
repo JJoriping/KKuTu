00f27fc885b840d654949abb34f214257b36306e
yeokyoomin <62054933+yeokyoomin@users.noreply.github.com>
2020-05-18 15:43:37 +0900

    Update const.js

--- Server/lib/const.js
+++ Server/lib/const.js
@@ -206,7 +206,7 @@ exports.KO_INJEONG = [
 	"MOB", "HYK", "CYP", "HRH", "STA", "OIJ",
 	"KGR", "ESB", "ELW", "OIM", "OVW", "NEX", /*"WOW",*/
 	"YRY", "KPO", "JLN", "JAN", "ZEL", "POK", "HAI",
-	"HSS", "KMV", "HDC", "HOS"
+	"HSS", "KMV", "HDC", "HOS", "YTB", "MCJ"
 ];
 exports.EN_INJEONG = [
 	"LOL"
