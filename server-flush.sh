a275207c3564fe9894d35457b36f81fe6e9ff208
젠하 <82743786+JNHA0307@users.noreply.github.com>
2021-04-25 13:50:35 +0900

    1,2 번째 줄에서 -r 말고 rm만 사용,

--- server-flush.sh
+++ server-flush.sh
@@ -1,5 +1,5 @@
-rm -rf "Server/run.bat"
-rm -rf "Server/lib/Web/lib/in_game_kkutu.js"
+rm "Server/run.bat"
+rm "Server/lib/Web/lib/in_game_kkutu.js"
 
 rm -rf Server/node_modules
 rm -rf Server/lib/node_modules
