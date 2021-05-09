128db73587748c184ff4c54147904883d1559bac
Chu Hyeonjin <choo51455145@gmail.com>
2021-05-09 20:15:55 +0900

    kdhkr님 의견 보완

--- Server/lib/Web/auth/auth_discord.js
+++ Server/lib/Web/auth/auth_discord.js
@@ -26,7 +26,6 @@ module.exports.strategy = (process, MainDB, Ajae) => {
         $p.id = $p.authType+"-"+profile.id;
         $p.name = profile.username;
         $p.title = profile.username;
-        $p.image = profile.avatar;
         $p.image = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}`;
 
         process(req, accessToken, MainDB, $p, done);
