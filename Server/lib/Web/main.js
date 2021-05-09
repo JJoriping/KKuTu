e65c844289854cff9800f642b218024b6e2f6979
베프 <lshqqytiger@naver.com>
2021-05-09 10:09:50 +0900

    불필요한 부분 삭제

--- Server/lib/Web/main.js
+++ Server/lib/Web/main.js
@@ -225,10 +225,6 @@ Server.get("/", function(req, res){
 
 		if($doc){
 			req.session.profile = $doc.profile;
-			if($doc){
-				if($doc.nickname && $doc.nickname != null) req.session.profile.title = req.session.profile.name = $doc.nickname;
-				req.session.nickname = req.session.profile.title;
-			};
 			id = $doc.profile.sid;
 		}else{
 			delete req.session.profile;
