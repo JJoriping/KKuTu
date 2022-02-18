f9c4a844665ef6cebd26fcaea4dde067e9fb65b9
IAMIHAH <kr.ihah@kakao.com>
2022-02-18 19:20:03 +0900

    📹 받아오는 값 변경

--- Server/lib/Web/auth/auth_twitch.js
+++ Server/lib/Web/auth/auth_twitch.js
@@ -1,7 +1,7 @@
 const config = require('../../sub/auth.json');
 
 module.exports.config = {
-    strategy: require('passport-twitch').Strategy,
+    strategy: require('passport-twitch-new').Strategy,
     color: '#5C4EB0',
     fontColor: '#FFFFFF',
     vendor: 'twitch',
@@ -20,13 +20,11 @@ module.exports.strategy = (process, MainDB, Ajae) => {
     return (req, accessToken, refreshToken, profile, done) => {
         const $p = {};
 
-        // var fullname = profile.username+"#"+profile.discriminator;
-
         $p.authType = "twitch";
         $p.id = $p.authType+"-"+profile.id;
-        $p.name = profile.username;
-        $p.title = profile.username;
-        $p.image = profile.avatar;
+        $p.name = profile.display_name;
+        $p.title = profile.display_name;
+        $p.image = profile.profile_image_url;
 
         process(req, accessToken, MainDB, $p, done);
     }
